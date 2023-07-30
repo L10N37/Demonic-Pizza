1bccd4ad3fe4d56b549aff1b49883342f28fd029
Lionel Sanderson <lfsanderson@hotmail.comgit config --global user.name Lionel>
2023-07-30 16:03:56 +1000

    mobile res scaling

--- client/src/App.js
+++ client/src/App.js
@@ -19,12 +19,13 @@ function App() {
 
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+         
           <div className="logoContainer">
             <img src={require('./assets/images/logo.png')} alt="Demonic Pizza Business Logo" id="logo-image" />
           </div>
 
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/pizza" element={<Pizza />} />
