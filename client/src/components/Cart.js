b49dcc5b9f7e254a0f98e7a242f2deb5c0b56325
Lionel Sanderson <lfsanderson@hotmail.comgit config --global user.name Lionel>
2023-07-31 11:44:35 +1000

    add login/checkout btn

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,15 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CartContext } from '../contexts/CartContext';
 
 const Cart = () => {
   const { cart } = useContext(CartContext);
+  const [delivery, setDelivery] = useState(false);
+  const deliveryFee = 12.99;
 
-  const totalCartPrice = cart.reduce((total, cartItem) => total + cartItem.totalPrice, 0);
+  const totalCartPrice = cart.reduce((total, cartItem) => total + cartItem.totalPrice, 0) + (delivery ? deliveryFee : 0);
+
+  const handleCheckout = () => {
+    console.log('Proceeding to checkout');
+  };
+
+  const handleDeliveryChange = () => {
+    setDelivery(!delivery);
+  };
+
+  const handleLogin = () => {
+    // Actual login handling will go here.
+    console.log('Login clicked');
+  };
 
   return (
     <div>
       {cart.length === 0 ? (
-        <p id = "cartEmptyText">Your cart is empty.</p>
+        <p id="cartEmptyText">Your cart is empty.</p>
       ) : (
         <div>
           <table>
@@ -38,6 +53,18 @@ const Cart = () => {
               </tr>
             </tbody>
           </table>
+          <div className="checkout-btn-container">
+            <input type="checkbox" id="delivery" name="delivery" checked={delivery} onChange={handleDeliveryChange} />
+            <label htmlFor="delivery">Add Delivery ($12.99)</label>
+          <br />
+          <br />
+            <button className="checkout-btn" onClick={handleCheckout}>Checkout</button>
+          </div>
+          <br />
+          <br />
+          <div className="login-btn-container">
+            <button className="login-btn" onClick={handleLogin}>Login</button>
+          </div>
         </div>
       )}
     </div>
