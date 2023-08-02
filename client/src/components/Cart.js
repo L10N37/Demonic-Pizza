11b0125407e614b62d65e11d98a8b3c42ce07610
Lionel Sanderson <lfsanderson@hotmail.comgit config --global user.name Lionel>
2023-08-02 16:29:37 +1000

    fix cart empty text logic

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,7 +1,5 @@
-//console.log(localStorage.getItem('token'));
-
 import React, { useState, useEffect } from 'react';
-import '../assets/css/SignInPage.css';                      // Styling
+import '../assets/css/SignInPage.css'; // Styling
 import { useStoreContext } from '../contexts/CartContext';
 import { useMutation } from '@apollo/client';
 import { SIGNUP_MUTATION, LOGIN_MUTATION } from '../utils/mutations';
@@ -15,13 +13,13 @@ const Cart = () => {
   const { cart } = state;
 
   const [signInData, setSignInData] = useState({ email: '', password: '' });
-  const [signUpData, setSignUpData] = useState({ 
-    firstName: '', 
-    lastName: '', 
-    email: '', 
-    address: { street: '', suburb: '', city: '', state: '', postcode: '' }, 
-    mobile: '', 
-    password: '' 
+  const [signUpData, setSignUpData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: { street: '', suburb: '', city: '', state: '', postcode: '' },
+    mobile: '',
+    password: '',
   });
 
   const [addUser] = useMutation(SIGNUP_MUTATION);
@@ -53,7 +51,7 @@ const Cart = () => {
     }
   };
 
-const handleSignUp = async (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
     try {
       const { data } = await addUser({ variables: { ...signUpData, address: { ...signUpData.address } } });
@@ -83,18 +81,17 @@ const handleSignUp = async (e) => {
       console.error(err);
       // handle error
     }
-  }; 
+  };
 
-useEffect(() => {
+  useEffect(() => {
     const isSignedInLocalStorage = localStorage.getItem('isSignedIn');
     setIsSignedIn(isSignedInLocalStorage === 'true'); // Set isSignedIn to true only if localStorage['isSignedIn'] is 'true'
   }, []);
 
-
   const handleSignOut = () => {
     dispatch({ type: 'CLEAR_CART' });
     localStorage.removeItem('isSignedIn');
-    // setIsSignedIn(false); // This line should be removed or set appropriately
+    window.location.reload(); // reload on sign out to reset the page
   };
 
   const [isSignedIn, setIsSignedIn] = useState(false); // Initialize isSignedIn as false
@@ -102,17 +99,15 @@ useEffect(() => {
   const totalCartPrice = cart.reduce((total, cartItem) => total + cartItem.totalPrice, 0);
   const totalCartPriceWithDelivery = isDelivery ? totalCartPrice + 12.99 : totalCartPrice;
 
-  useEffect(() => {
-    const isSignedInLocalStorage = localStorage.getItem('isSignedIn');
-    setIsSignedIn(!!isSignedInLocalStorage);
-  }, []);
-
   return (
     <div className="signInContainer">
+      {/* Check if the user is signed in and the cart has items */}
       {isSignedIn && cart.length > 0 && (
         <div>
           <h2>Cart Contents</h2>
+          {/* Render the cart contents in a table */}
           <table>
+            {/* Table header */}
             <thead>
               <tr>
                 <th>Product Name</th>
@@ -123,21 +118,24 @@ useEffect(() => {
                 <th>Extras</th>
               </tr>
             </thead>
+            {/* Table body */}
             <tbody>
-            {cart.map((item) => (
-              <tr key={item.id}>
-                <td>{item.name}</td>
-                <td>{item.purchaseQuantity}</td>
-                <td>${item.totalPrice ? (item.totalPrice / item.purchaseQuantity).toFixed(2) : ''}</td>
-                <td>${item.totalPrice ? item.totalPrice.toFixed(2) : ''}</td>
-                <td>{item.crust || ''}</td>
-                <td>{item.extras ? item.extras.join(', ') : ''}</td>
-              </tr>
-            ))}
+              {/* Map through the cart items and display them in rows */}
+              {cart.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.purchaseQuantity}</td>
+                  <td>${item.totalPrice ? (item.totalPrice / item.purchaseQuantity).toFixed(2) : ''}</td>
+                  <td>${item.totalPrice ? item.totalPrice.toFixed(2) : ''}</td>
+                  <td>{item.crust || ''}</td>
+                  <td>{item.extras ? item.extras.join(', ') : ''}</td>
+                </tr>
+              ))}
+              {/* Additional row for the delivery option */}
               <tr>
                 <td>
                   <label htmlFor="delivery">
-                    <input 
+                    <input
                       type="checkbox"
                       id="delivery"
                       checked={isDelivery}
@@ -153,35 +151,42 @@ useEffect(() => {
           </table>
         </div>
       )}
-  
+
+      {/* If the user is not signed in and not showing the sign-up form, show the sign-in form */}
       {!isSignedIn && !showSignUp && (
-        <SignInForm 
-          signInData={signInData} 
+        <SignInForm
+          signInData={signInData}
           handleSignInChange={handleSignInChange}
-          handleSignIn={handleSignIn} 
-          setShowSignUp={setShowSignUp} 
+          handleSignIn={handleSignIn}
+          setShowSignUp={setShowSignUp}
         />
       )}
-  
+
+      {/* If the user is not signed in and showing the sign-up form, show the sign-up form */}
       {!isSignedIn && showSignUp && (
-        <SignUpForm 
-          signUpData={signUpData} 
+        <SignUpForm
+          signUpData={signUpData}
           handleSignUpChange={handleSignUpChange}
-          handleSignUp={handleSignUp} 
-          setShowSignUp={setShowSignUp} 
+          handleSignUp={handleSignUp}
+          setShowSignUp={setShowSignUp}
           isSignedIn={isSignedIn} // Pass isSignedIn to SignUpForm
         />
       )}
-  
+
+      {/* If the user is signed in, display a message and a "Sign Out" button */}
       {isSignedIn && (
         <div>
           <p>You are logged in.</p>
           <button id="signOutButton" onClick={handleSignOut}>Sign Out</button>
         </div>
       )}
+
+      {/* If the user is signed in but the cart is empty, show "Your cart is empty" */}
+      {isSignedIn && cart.length === 0 && (
+        <p id = "cartEmptyText">Your cart is empty</p>
+      )}
     </div>
   );
-}
+};
 
 export default Cart;
-
